0d51a810332076caa5535c2444939599b6687df5
ErtugrulEryildiz <60051584+ErtugrulEryildiz@users.noreply.github.com>
2021-01-05 13:02:15 -0500

    Update sfmm.c

--- Dynamic Memory Allocator/src/sfmm.c
+++ Dynamic Memory Allocator/src/sfmm.c
@@ -1,8 +1,3 @@
-/**
- * All functions you make for the assignment must be implemented in this file.
- * Do not submit your assignment with a main function in this file.
- * If you submit with a main function in this file, you will get a zero.
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
